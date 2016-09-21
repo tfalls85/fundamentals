10a254d01dd120b89d6a8100a7dbf4ede267b2e4
Travis Falls <Travis@Kadys-MacBook-Pro.local>
2016-09-21 17:00:53 -0400

    Added more JS practice & store front updates

---
+++ jsbasics/0.06_functions.js
@@ -0,0 +1,83 @@
+
+// //Declaration
+// function helloWorld() {
+// 	console.log("Hello World");
+// }
+
+// //Call
+// helloWorld();
+
+// function printMyName() {
+// 	console.log("Travis Falls");
+// }
+
+// printMyName();
+
+// //Declaration #2
+// var name = function myName() {
+// 	console.log("Travis Falls");
+// }
+
+// //Call
+// name();
+
+// //Scope
+// function addNumbers() {
+// 	var x = 10;
+// 	var y = 10;
+// 	console.log(x + y);
+// }
+
+// addNumbers();
+
+// //Practice
+// function multiplyNumbers() {
+// 	var x = 11;
+// 	var y = 9;
+// 	console.log(x * y);
+// }
+
+// multiplyNumbers();
+
+// //Parameters & Arguments
+// function randomNumber (x) {
+// 	console.log("The number you entered was : " + x);
+// }
+
+// randomNumber(6);
+
+// function bankPin(x) {
+// 	var cardPin = 5678;
+
+// 	if (cardPin != x) {
+// 		console.log("Try again!");
+// 	} else {
+// 	console.log("You are correct!");
+// }
+// }
+// bankPin(5678);
+
+// function numAdd(a, b) {
+// 	return a + b;
+// }
+
+// console.log(numAdd(1, 2));
+// console.log(numAdd("Travis", " Falls"));
+
+
+//Bronze
+function myNameIs () {
+	var firstName = "Travis";
+	var lastName = "Falls";
+	console.log(firstName + " " + lastName);
+}
+
+myNameIs();
+
+
+//Silver
+function addBills (mortgage, phone, internet) {
+	return mortgage + phone + internet
+}
+
+console.log(addBills(800, 150, 25));
