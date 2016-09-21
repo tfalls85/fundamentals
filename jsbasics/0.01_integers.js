12f576b6ad25d66b208ba4b49f24f347911c2f11
Travis Falls <Travis@Kadys-MacBook-Pro.local>
2016-09-21 10:28:47 -0400

    Updated Store App and added JS Basics folder

---
+++ jsbasics/0.01_integers.js
@@ -0,0 +1,60 @@
+// var num = 7;
+// var newNum = 5; // this is camel casing
+// var newer_num = 10; //this is snake casing
+
+//console.log() prints to the console
+
+//operations with console.log
+// console.log(5 + 5);
+//TODO - make a subtraction, multiplication and division console.log()
+// console.log(10 - 5);
+// console.log(5 * 5);
+// console.log(100 / 25);
+
+//Modulus Operator
+//% returns remainder
+// console.log(10 % 5); //returns zero
+// console.log(4 % 3); //returns one
+
+//TODO - make a modulus that reutrns zero and another that returns a remainder
+// console.log(100 % 5);
+// console.log(100 % 7);
+
+// var num = 1 + 1;
+// console.log(num);
+
+// var a = 256;
+// var b = 92;
+// console.log(a * b);
+
+// var c = a + b;
+// console.log(c);
+
+var newNum = 10;
+newNum =  newNum + 1
+console.log(newNum);
+
+newNum++;
+console.log(newNum);
+
+newNum = newNum - 1
+console.log(newNum);
+
+newNum--;
+console.log(newNum);
+
+newNum = newNum + 10;
+console.log(newNum);
+
+newNum += 10;
+console.log(newNum);
+
+newNum = newNum - 10;
+console.log(newNum);
+
+newNum -= 10;
+console.log(newNum);
+
+newNum /= 10;
+console.log(newNum);
+
