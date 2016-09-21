cd0b8fc600a4b20e80557152c8a52c7ffcd2f5bd
Travis Falls <Travis@Kadys-MacBook-Pro.local>
2016-09-21 11:19:41 -0400

    Added JS string files

---
+++ jsbasics/0.02_strings.js
@@ -0,0 +1,32 @@
+var birthCity = "South Bend";
+var birthState = "Indiana";
+// console.log(birthCity);
+
+var username = "travisfalls";
+var password = "qwerty1234";
+var tweet = "Kanye is broke again";
+var facebookPost = "I love coding!";
+var instaCaption = "#Awesome";
+
+// console.log("username");
+// console.log(username);
+// console.log("Hello World");
+
+//Concatenation
+// console.log(birthCity, birthState);
+// console.log(birthCity + birthState);
+// console.log(birthCity + ", " + birthState);
+
+var gradYear = 2004;
+var highSchool = "Mishawaka High School";
+
+console.log("I graduated from " + highSchool + " in " + gradYear + ".");
+
+
+var make = "Ford ";
+var model = "Freestyle ";
+var year = 2006;
+
+var myCar= make + model + year;
+
+console.log(myCar);
